469ec3de79bfb49df5363279c9d99f28d3e3ee2f
Rodrigo Soares <adsrodsoares@gmail.com>
2021-03-21 14:06:09 -0300

    Working with Arrays

---
+++ 05-arrays.rb
@@ -0,0 +1,40 @@
+list = []
+
+p list.class;
+p list.length;
+
+list << 42; #It includes a new value in the end of the Array.
+
+p list;
+
+list << 42; #You may include the same value as times as you want.
+
+p list;
+
+list << "Rodrigo" #You may include any Object type in the Array
+
+p list;
+
+
+list << true #You may include any Object type in the Array
+
+p list;
+
+p list.first; #It returns the first element from the list
+
+p list;
+
+p list.last; #It returns the last element from the list
+
+p list;
+
+p list.empty?; #It returns if the list is empty or not, with a true or false;
+
+list1 = [1, 2, 3]
+
+list2 = [3, 4, 5, 6]
+
+p list1 + list2; #You may join two lists with the method +;
+p list1 - list2; #You may dicrease the repeated member from the first list with the method -;
+p list1 * list2;
+
