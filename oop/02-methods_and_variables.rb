cb7ef0241b5546bbc71b54f6acae3a98f1a5b3c9
Rodrigo Soares <adsrodsoares@gmail.com>
2021-03-24 22:47:17 -0300

    Using attr_writer to set name

--- oop/02-methods_and_variables.rb
+++ oop/02-methods_and_variables.rb
@@ -1,4 +1,5 @@
 class Person
+    attr_writer :name
     
     def initialize(name)
         @name = name
@@ -8,14 +9,7 @@ class Person
         puts "Hello #{@name}"
     end
 
-    def name=(new_name)
-        @name = new_name
-    end
 end
 
 rodrigo = Person.new("rodrigo")
 puts rodrigo.print_hello
-
-rodrigo.name = "Rodrigo Soares"
-
-rodrigo.print_hello
