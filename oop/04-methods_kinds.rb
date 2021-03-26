f0cf2ed6f14a8fb42b87f8be24152d9742cde8c3
Rodrigo Soares <adsrodsoares@gmail.com>
2021-03-26 20:37:02 -0300

    Protected and private methods acess

--- oop/04-methods_kinds.rb
+++ oop/04-methods_kinds.rb
@@ -1,6 +1,8 @@
 class MyClass
     def m1
         puts "Method 1"
+        m2
+        m3
     end
     
     private 
@@ -12,11 +14,25 @@ class MyClass
     def m3
         puts "Method 3"
     end
+
+    protected 
+    def m5
+        puts "Method 5"
+    end
 end
+
+    class MySubClass < MyClass
+        def m4
+            m3
+            another_object = MyClass.new
+            puts "Method 4"
+            another_object.m5
+        end
+    end
 
 ###From here ahead, is another context
 
-obj = MyClass.new
-obj.m1
-obj.m2 #Now m2 and m3 are private methods and won't be read out of class scope.
-obj.m3
+obj = MySubClass.new
+obj.m4
+obj.m5 ##Will not work because it cannot be read out of the package (m5 is a protected method)
+
